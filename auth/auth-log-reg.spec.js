2f930ad1eb5a1af0a564449947e05d858b1b9758
coryortega <coryortega3@gmail.com>
2020-01-07 14:56:57 -0800

    added testing

---
+++ auth/auth-log-reg.spec.js
@@ -0,0 +1,43 @@
+const request = require("supertest");
+const server = require("../api/server");
+
+// Test for registering a new user
+describe("auth-log-reg", () => {
+  describe("/register and /login", () => {
+    it("should allow the user to create an account", async () => {
+      await request(server)
+        .post("/api/auth/register")
+        .send({
+          username: "TestingAccount",
+          password: "TestingPassword",
+          email: "TestingAccount@email.com"
+        })
+        .expect(201);
+    });
+    it("should not allow the user to create an account", async () => {
+      await request(server)
+        .post("/api/auth/register")
+        .send({
+          username: "TestingAccount"
+        })
+        .expect(400);
+    });
+    it("should allow the user to signin to account", async () => {
+      await request(server)
+        .post("/api/auth/login")
+        .send({
+          username: "TestingAccount",
+          password: "TestingPassword"
+        })
+        .expect(200);
+    });
+    it("should not allow the user to signin to account", async () => {
+      await request(server)
+        .post("/api/auth/login")
+        .send({
+          username: "Testing Account"
+        })
+        .expect(401);
+    });
+  });
+});
--- knexfile.js
+++ knexfile.js
@@ -21,6 +21,25 @@ module.exports = {
     }
   },
 
+  testing: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./database/testing.db3"
+    },
+    migrations: {
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    }
+  },
+
   production: {
     client: "pg",
     connection: process.env.DATABASE_URL,
